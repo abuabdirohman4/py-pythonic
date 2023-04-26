06c726307053b8f713a0e06a95116036b23a4648
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-04-26 17:38:13 +0700

    Pythonic 03 | Melakukan Packing Sekumpulan Nilai dalam List

---
+++ 03_packing_list.py
@@ -0,0 +1,15 @@
+# Conventional
+# a = 10
+# b = 20
+# c = 30
+# d = [None]*3 # kapasitasnya 3
+# d[0] = a
+# d[1] = b
+# d[2] = c
+
+# Pythonic
+a, b, c = 10, 20, 30
+d = [a, b, c]
+
+print(f'a={a}, b={b}, c={c}')
+print(f'd={d}')
